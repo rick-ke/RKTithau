9f677daf38ee7c2bca39dbfc6f41581074115b35
Rick's mac <rueisiang.ke@gmail.com>
2020-03-15 00:39:18 +0800

    initial project

---
+++ RKTithau/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  RKTithau
+//
+//  Created by Rick Ke on 2020/2/26.
+//  Copyright © 2020 RickKe. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+}
